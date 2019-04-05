08592e37df0b667675c0780fef59748d57f25104
obulygin <obulygin@intermedia.net>
2019-04-05 12:18:20 +0300

    Задание 25, 26

---
+++ fixture/application.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+from fixture.project import Helper_project
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
+from fixture.soap import SoapHelper
+
+
+class Application:
+
+
+    def __init__(self, browser, config):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False}, executable_path="C:\SeleniumDrivers\geckodriver.exe")
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome(executable_path="C:\SeleniumDrivers\chromedriver.exe")
+        elif browser == "ie":
+            self.wd = webdriver.Ie(executable_path="C:\SeleniumDrivers\IEDriverServer.exe")
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(0.5)
+        self.session = SessionHelper(self)
+        self.project = Helper_project(self)
+        self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+        self.soap = SoapHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_url)
+
+
+    def destroy (self):
+        self.wd.quit()
+
+
+
+
